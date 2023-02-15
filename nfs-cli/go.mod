83fe434a08f7ecef662f4a58a2668d84662d17f9
raonismaneoto <raoni.smaneoto@vtex.com.br>
2023-02-15 18:04:28 -0300

    mount working

--- nfs-cli/go.mod
+++ nfs-cli/go.mod
@@ -1,3 +1,16 @@
 module github.com/raonismaneoto/custom-nfs-server/nfs-cli
 
 go 1.18
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/raonismaneoto/custom-nfs-server/nfs-client v0.0.0-20230215190024-57dbd7452c36 // indirect
+	github.com/raonismaneoto/custom-nfs-server/nfs-server v0.0.0-20230215132254-4ea9e8ddb937 // indirect
+	golang.org/x/exp v0.0.0-20230213192124-5e25df0256eb // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
--- nfs-cli/main.go
+++ nfs-cli/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/raonismaneoto/custom-nfs-server/nfs-cli/models"
+	client "github.com/raonismaneoto/custom-nfs-server/nfs-client"
 )
 
 func main() {
@@ -15,10 +21,60 @@ func main() {
 
 	switch command := args[0]; command {
 	case "mount":
+		if len(args) != 3 {
+			panic("Usage: mount <origin> <absolute_destination>")
+		}
 		log.Println("exec mount command")
+		client := client.NewClient(os.Getenv("server_addr"))
+		cmd := exec.Command("whoami")
+		out, err := cmd.Output()
+		if err != nil {
+			panic("error while getting username: " + err.Error())
+		}
+		username := string(out)
+		username = strings.Replace(username, "\n", "", -1)
+		cmd = exec.Command("hostname")
+		out, err = cmd.Output()
+		if err != nil {
+			panic("error while getting hostname: " + err.Error())
+		}
+		hostname := string(out)
+		hostname = strings.Replace(hostname, "\n", "", -1)
+		path := args[1]
+		response, err := client.Mount(username+"@"+hostname, path)
+		if err != nil {
+			panic("error while mounting request: " + err.Error())
+		}
+		absDestPath := args[2]
 
+		var filesMd []models.Metadata
+		err = json.Unmarshal(response, &filesMd)
+		if err != nil {
+			panic("error unmarshalling mount response. Error: " + err.Error())
+		}
+
+		for _, fmd := range filesMd {
+			func(currFmd models.Metadata) {
+				fm, err := os.OpenFile(absDestPath+"/"+currFmd.Path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+				if err != nil {
+					panic("unable to open/create " + absDestPath + ". Error: " + err.Error())
+				}
+				defer fm.Close()
+				parsedFmd, err := json.Marshal(currFmd)
+				if err != nil {
+					panic(err.Error())
+				}
+				if _, err := fm.Write(parsedFmd); err != nil {
+					panic("unable to write to: " + absDestPath + ". Error: " + err.Error())
+				}
+			}(fmd)
+		}
 	case "save":
 		log.Println("exec save command")
+		if len(args) != 2 {
+			panic("Usage: save <absolute_origin> <destination>")
+		}
+
 	case "read":
 		log.Println("exec read command")
 	case "chpem":
