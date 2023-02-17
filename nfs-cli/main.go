d84a880423227f922fad54cadbf13b9cb2084c1c
raonismaneoto <raoni.smaneoto@vtex.com.br>
2023-02-17 10:53:26 -0300

    refactoring + install script

--- nfs-cli/main.go
+++ nfs-cli/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"os/exec"
 	"strings"
 
-	"github.com/raonismaneoto/custom-nfs-server/helpers"
+	"github.com/raonismaneoto/custom-nfs-server/nfs-cli/commands"
 	"github.com/raonismaneoto/custom-nfs-server/nfs-cli/models"
 	client "github.com/raonismaneoto/custom-nfs-server/nfs-client"
 )
@@ -24,6 +22,12 @@ func main() {
 	}
 
 	client, username, hostname := getCmdsCommonData()
+	cconfig := models.CommandConfiguration{
+		Username:           username,
+		Hostname:           hostname,
+		Client:             *client,
+		MaxBytesPerRequest: MaxBytesPerRequest,
+	}
 
 	switch command := args[0]; command {
 	case "mount":
@@ -33,65 +37,17 @@ func main() {
 		log.Println("exec mount command")
 
 		path := args[1]
-		response, err := client.Mount(username+"@"+hostname, path)
-		if err != nil {
-			panic("error while mounting request: " + err.Error())
-		}
 		absDestPath := args[2]
-
-		var filesMd []models.Metadata
-		err = json.Unmarshal(response, &filesMd)
-		if err != nil {
-			panic("error unmarshalling mount response. Error: " + err.Error())
-		}
-
-		for _, fmd := range filesMd {
-			func(currFmd models.Metadata) {
-				fm, err := os.OpenFile(absDestPath+"/"+currFmd.Path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-				if err != nil {
-					panic("unable to open/create " + absDestPath + ". Error: " + err.Error())
-				}
-				defer fm.Close()
-				parsedFmd, err := json.Marshal(currFmd)
-				if err != nil {
-					panic(err.Error())
-				}
-				if _, err := fm.Write(parsedFmd); err != nil {
-					panic("unable to write to: " + absDestPath + ". Error: " + err.Error())
-				}
-			}(fmd)
-		}
+		commands.ExecMount(path, absDestPath, cconfig)
 	case "save":
 		log.Println("exec save command")
 		if len(args) != 3 {
 			panic("Usage: save <absolute_origin> <destination>")
 		}
-		content := make(chan []byte)
-		proceed := make(chan string)
+
 		origin := args[1]
 		destination := args[2]
-		go client.Save(username+"@"+hostname, destination, content, proceed)
-		f, err := os.Open(origin)
-		if err != nil {
-			panic("unable to open file")
-		}
-		defer f.Close()
-		stat, err := f.Stat()
-		if err != nil {
-			panic("unable to get file stat")
-		}
-		chuncks := int32(math.Ceil(float64(stat.Size()) / float64(MaxBytesPerRequest)))
-		for i := int32(0); i < chuncks; i++ {
-			<-proceed
-			data, err := helpers.ReadFileChunk(origin, i*MaxBytesPerRequest, MaxBytesPerRequest)
-			if err != nil {
-				panic("error while reading file chunk: " + err.Error())
-			}
-			content <- data
-		}
-		<-proceed
-		close(content)
-		<-proceed
+		commands.ExecSave(origin, destination, cconfig)
 	case "read":
 		log.Println("exec read command")
 		if len(args) != 3 {
@@ -99,46 +55,17 @@ func main() {
 		}
 
 		localPath := args[2]
-		_, err := os.Stat(localPath)
-		if err == nil {
-			err := os.Remove(localPath)
-			if err != nil {
-				panic(err.Error())
-			}
-		}
-		f, err := os.OpenFile(localPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			panic("unable to open/create file")
-		}
-
-		content := make(chan []byte)
-		proceed := make(chan string)
-
-		go client.Read(username+"@"+hostname, args[1], content, proceed)
-
-		for {
-			proceed <- "proceed"
-
-			currContent, ok := <-content
-			if !ok {
-				close(proceed)
-				f.Close()
-				return
-			}
-
-			if _, err := f.Write(currContent); err != nil {
-				log.Println("unable to write to file")
-			}
-		}
+		remotePath := args[1]
+		commands.ExecRead(localPath, remotePath, cconfig)
 	case "chpem":
 		log.Println("exec chpem command")
 	default:
-		fmt.Printf("Usage: \nexecutable <command> <args> [options]")
+		fmt.Printf("Usage: nfs <command> <args> [options]")
 	}
 }
 
 func getCmdsCommonData() (*client.Client, string, string) {
-	client := client.NewClient(os.Getenv("server_addr"))
+	client := client.NewClient(os.Getenv("custom_nfs_server_addr"))
 	cmd := exec.Command("whoami")
 	out, err := cmd.Output()
 	if err != nil {
